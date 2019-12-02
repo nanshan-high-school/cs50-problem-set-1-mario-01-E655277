27569c4a76f16c349354c81c572f13ba7c18560e
E655277 <54939685+E655277@users.noreply.github.com>
2019-12-02 23:11:34 +0800

    Create 27033.cpp

---
+++ 27033.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int times;
+    do {
+        cout << "高度:";
+        cin >> times;
+       } while(times < 1 || times > 8);
+
+       for(int t = 0; t < times;t++) {
+           for (int n = 0; n <= t;n++){
+                cout << "#";
+           }
+           cout << "\n";
+       }      
+}
